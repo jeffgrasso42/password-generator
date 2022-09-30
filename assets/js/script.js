7d112cf416d1e504de0e3e55f2d93cab6699c75d
JAG <jagrasso42@gmail.com>
2022-09-29 21:54:55 -0400

    added functions generatePassword(), getLength(), and checkLength() to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,8 +13,36 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
+function generatePassword() {
+  var password = "";
+  var length = getLength();
+}
+
+// get password length from the user
+function getLength() {
+  //prompt the user for the length of the password (between 8 and 128)
+  var length = prompt("Enter desired password length (min 8, max 128): ")
+  // if checkLength() function returns true, length is valid
+  if (checkLength(length)) {
+    return length;
+  // if length is invalid, invoke getLength again
+  } else {
+    alert("Error: Invalid input, try again");
+    getLength();
+  }
+}
+
+//validates length of user input
+function checkLength(length) {
+  if(length > 7 && length < 129) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
